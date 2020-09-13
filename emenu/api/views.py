d3c1a959754d5469e2f29af42cdd6a98bafe7ec2
Kamil Kurzacz <kamil.kurzacz@gmail.com>
2020-09-13 21:01:06 +0200

    Basic public menu view

--- emenu/api/views.py
+++ emenu/api/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from api.serializers import MenuSerializer
+from api.models import Menu
+from rest_framework.generics import GenericAPIView
+from rest_framework import mixins
 
-# Create your views here.
+
+class MenusView(mixins.ListModelMixin, GenericAPIView):
+    """ View to list menus in the app. """
+
+    serializer_class = MenuSerializer
+    queryset = Menu.objects.all()
+
+    def get(self, request, *args, **kwargs):
+        """ Returns a list of menus. """
+        return self.list(request, *args, **kwargs)
