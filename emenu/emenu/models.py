32167132715fcd04a292eed5222d26a66ad2b358
Kamil Kurzacz <kamil.kurzacz@gmail.com>
2020-09-13 18:47:17 +0200

    Create models.py

---
+++ emenu/emenu/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+
+class Menu(models.Model):
+    name = models.CharField(max_length=63, unique=True)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Dish(models.Model):
+    menu = models.ForeignKey(Menu, on_delete=models.CASCADE)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=4, decimal_places=2)
+    prepare_time = models.IntegerField()  # in minutes
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_vegan = models.BooleanField()
