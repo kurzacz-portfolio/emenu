9c77fb25487bfae7444eb087216567629c763e62
Kamil Kurzacz <kamil.kurzacz@gmail.com>
2020-09-14 20:12:45 +0200

    Add menus list ordering

--- emenu/api/views.py
+++ emenu/api/views.py
@@ -2,6 +2,7 @@ from api.serializers import MenuSerializer
 from api.models import Menu
 from rest_framework.generics import ListAPIView
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters
 
 
 class MenusListView(ListAPIView):
@@ -9,6 +10,6 @@ class MenusListView(ListAPIView):
 
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
-    filter_backends = [DjangoFilterBackend]
+    filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
     filterset_fields = ['name', 'created_at', "updated_at"]
-
+    ordering_fields = ['name']
